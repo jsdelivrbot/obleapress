8e837c5fce3b0ad88450f155c9b98a7b2affbafd
David G <davigarcas@gmail.com>
2017-06-21 11:40:58 -0500

    initial commit

---
+++ header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<link href="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.css" rel="stylesheet">
+	<title></title>
+	<?php wp_head(); ?>
+</head>
+<body>
